95210b13cbd1f8991fe01e88757233497276dc11
2san <chillout2san@gmai.com>
2021-02-14 13:53:18 +0900

    2/14-1

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
+        <tr v-for="(todo,index) in Todos" v-bind:key="index">
+            <th>{{ index }}</th>
+            <th>{{ todo.task }}</th>
+            <th><button>{{ todo.status }}</button></th>
+            <th><button>削除</button></th>
         </tr>
       </tbody>
     </table>
    <h2>新規タスクの追加</h2>
-   <input type="text"><button @click="log">追加</button>
+   <input type="text" v-model="task"><button @click="log">追加</button>
 
  </div>
 </template>
@@ -33,12 +33,18 @@
 export default {
   data() {
     return {
-      Todos: ['テスト']
+      task: '',
+      status: '作業中',
+      Todos: []
     }
   },
   methods: {
     log: function() {
-      console.log(this.Todos);
+      const todoElm = {
+        task: this.task,
+        status: this.status
+      };
+      this.Todos.push(todoElm);
     }
   }
 };
